fb1292863d89a2c3bcb543bb45189e951c7352d6
Norberto682 <99997260+Norberto682@users.noreply.github.com>
2022-05-20 19:17:30 -0500

    Tarea6_1906274

--- Tarea6_1906274.py
+++ Tarea6_1906274.py
@@ -1,7 +1,11 @@
+#Se importa los modulos necesarios previamente instalados en cmd como pip install python-nmap
 import nmap
+#Se realiza el inicio y limite de los puertos a escanear
 begin=10
 end=40
+#Se define la ip o maquina a escanear
 target='192.168.0.1'
+#Se procede a realizar el proceso de escaneo
 scanner=nmap.PortScanner()
 for i in range(begin,end+1):
  res = scanner.scan(target,str(i))
